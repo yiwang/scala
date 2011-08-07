fe5a6e641f82c9c44f054fc77b812ad662576dce
Yi Wang <e@yi-wang.me>
2011-08-07 00:01:30 -0400

    case class, pattern matching

---
+++ ScalaTutorial/Complex.scala
@@ -0,0 +1,13 @@
+class Complex(real: Double, imaginary: Double){
+    def re=real
+    def im=imaginary
+    override def toString()="" + re + (if(im<0) "" else "+") + im + "i"
+}
+
+object ComplexNumbers{
+    def main(args: Array[String]){
+        val c=new Complex(1.2, 3.4)
+        println("imaginary part: " + c.im)
+        println(c)
+    }
+}
